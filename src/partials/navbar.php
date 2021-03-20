6ca20c03463dfd092e219b849544629803933d91
damilolarandolph <damilolarandolph@gmail.com>
2021-03-20 23:19:56 +0000

    add logo to navbar, added links.

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,13 +1,17 @@
 <nav class="head-nav">
-    <h2>Evently</h2>
+    <div class="logo">
+        <a href="/">
+            <img src="/assets/images/logo.png" />
+        </a>
+    </div>
     <div class="search">
         <input placeholder="Search for events...." type="text" />
     </div>
     <div>
         <div class="button-group">
-            <a title="Login as user or organizer" href="">Login</a>
-            <a title="Register as a user" href="">Register</a>
-            <a title="Join Evently as an organizer" href="">Become an Organizer</a>
+            <a title="Login as user or organizer" href="/signin.php">Login</a>
+            <a title="Register as a user" href="/signup.php">Register</a>
+            <a title="Join Evently as an organizer" href="/org-signup.php">Become an Organizer</a>
         </div>
         <!-- <button class="avatar focus-popover-menu">
             <img class="avatar-img" src="/assets/images/placeholder_avatar.jpg">
