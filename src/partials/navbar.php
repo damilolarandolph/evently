0379f4930f931efea5992fa95d93fd57c3e8c0fa
damilolarandolph <damilolarandolph@gmail.com>
2021-03-20 13:11:54 +0000

    refactored css

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,20 +1,20 @@
-<nav>
+<nav class="head-nav">
     <h2>Evently</h2>
     <div class="search">
         <input placeholder="Search for events...." type="text" />
     </div>
     <div>
-        <!-- <div class="button-group">
+        <div class="button-group">
             <a title="Login as user or organizer" href="">Login</a>
             <a title="Register as a user" href="">Register</a>
             <a title="Join Evently as an organizer" href="">Become an Organizer</a>
-        </div> -->
-        <button class="avatar focus-popover-menu">
+        </div>
+        <!-- <button class="avatar focus-popover-menu">
             <img class="avatar-img" src="/assets/images/placeholder_avatar.jpg">
             <ul class="menu">
                 <li><a href="">Account</a></li>
                 <li><a href="">Logout</a></li>
             </ul>
-        </button>
+        </button> -->
     </div>
 </nav>
