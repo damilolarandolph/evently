a7f7c9eee7d1fce985761fabb15c5713e8982938
damilolarandolph <damilolarandolph@gmail.com>
2021-03-20 13:11:29 +0000

    finished add event page

---
+++ add-event.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/all.min.css" />
+    <link rel="stylesheet" href="./assets/css/common.css" />
+    <link rel="stylesheet" href="./assets/css/add-event.css" />
+
+    <title>Add Event</title>
+</head>
+
+<body>
+    <?php
+    include_once __DIR__ . "/src/partials/navbar.php";
+    ?>
+
+    <section>
+        <h2 class="heading">Create Event</h2>
+        <form id="add-form" class="form add-event-form">
+            <div class="event-title">
+                <input placeholder="event title..." name="title" type="text" />
+            </div>
+            <div class="form-group event-description">
+                <label>Description</label>
+                <textarea>event description</textarea>
+            </div>
+            <div class="form-group">
+                <label>Event image link</label>
+                <input placeholder="please enter image link" name="image-link" type="url" />
+            </div>
+            <div class="form-groups">
+                <div class="form-group">
+                    <label>Start Date</label>
+                    <input type="date" placeholder="enter start date" name="start-date" />
+                </div>
+                <div class="form-group">
+                    <label>Start Time</label>
+                    <select name="start-time" class="time-list">
+                    </select>
+                </div>
+            </div>
+            <div class="form-groups">
+                <div class="form-group">
+                    <label>End Date</label>
+                    <input type="date" placeholder="enter start date" />
+                </div>
+                <div class="form-group">
+                    <label>End Time</label>
+                    <select class="time-list">
+                    </select>
+                </div>
+            </div>
+            <div class="form-groups">
+                <div class="form-group">
+                    <label>Speaker</label>
+                    <input name="speaker" />
+                </div>
+                <div class="form-group">
+                    <label>Number of Tickets</label>
+                    <input name="num-tickets" type="number" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label>
+                    Location
+                </label>
+                <ul for="venue-tabs" class="inline form-groups radio-tabs">
+                    <label class="radio">
+                        <input type="radio" name="venue" />
+                        <div class="content">
+                            <i class="fas fa-check"></i>
+                            Venue
+                        </div>
+                    </label>
+                    <label class="radio">
+                        <input type="radio" name="venue" />
+                        <div class="content">
+                            <i class="fas fa-check"></i>
+                            Online
+                        </div>
+                    </label>
+                </ul>
+                <ul id="venue-tabs" class="tabs">
+                    <div class="form-group">
+                        <input placeholder="type to search for locations" id="map-suggestions" required type="text" name="location" />
+                        <input type="hidden" name="lat" />
+                        <input type="hidden" name="long" />
+                    </div>
+                    <div class="form-group">
+                        <input placeholder="Please enter meeting link, e.g zoom, skype, google meet" required type="text" name="location" />
+                    </div>
+                </ul>
+            </div>
+
+        </form>
+    </section>
+
+</body>
+<script src="/assets/js/common.js"></script>
+<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmM_a-0sPL6k12genP_RVor9iJ1AEupwI&libraries=places&callback=startMap">
+</script>
+<script>
+    let searchBox;
+    let form = document.getElementById("add-form")
+
+    function startMap(args) {
+        let input = document.getElementById("map-suggestions")
+        searchBox = new google.maps.places.SearchBox(input)
+        console.log(searchBox)
+
+        searchBox.addListener("places_changed", () => {
+            const places = searchBox.getPlaces()
+            form.elements["lat"].value = places[0].geometry.location.lat()
+            form.elements['long'].value = places[0].geometry.location.lng()
+            console.log(form.elements["lat"].value)
+        })
+
+    }
+    (function() {
+
+        const timeLists = document.querySelectorAll(".time-list");
+        let innerHTML = ""
+        for (let start = 0; start < 24; ++start) {
+            innerHTML += `<option> ${start < 10 ? '0':''}${start}:00 </option>`
+            innerHTML += `<option>${start < 10 ? '0':''}${start}:30 </option>`
+        }
+
+        for (let list of timeLists) {
+            list.innerHTML = innerHTML
+        }
+
+    })();
+</script>
+
+
+</html>
