ebb5715163b850d1bb07080f6e8900924be87e02
damilolarandolph <damilolarandolph@gmail.com>
2021-03-24 21:38:01 +0000

    refactoring.

--- src/data/models/organizer.php
+++ src/data/models/organizer.php
@@ -5,11 +5,28 @@ require_once __DIR__ . "/./user.php";
 class Organizer extends Model
 {
 
+    /** @var User */
     public $user;
     public $name;
 
     public function __construct()
     {
         parent::__construct("organizer");
+    }
+
+    /**
+     * 
+     * @var User $user
+     * @var string $name
+     * 
+     * @return Organizer
+     */
+    public static function withArgs($user, $name)
+    {
+        $organizer = new self;
+        $organizer->user = $user;
+        $organizer->name = $name;
+
+        return $organizer;
     }
 }
--- src/data/models/person.php
+++ src/data/models/person.php
@@ -1,16 +1,40 @@
 <?php
 
 require_once __DIR__ . "/./model.php";
+require_once __DIR__ . "/user.php";
 
 class Person extends Model
 {
+    /** @var User */
     public $user;
+
+    /** @var string */
     public $firstName;
+
+    /** @var string */
     public $lastName;
 
     public function __construct()
     {
         parent::__construct("person");
+    }
+
+    /**
+     * 
+     * @param User $user
+     * @param string $firstName
+     * @param string $lastName
+     * 
+     * @return Person
+     * 
+     */
+    public static function withArgs($user, $firstName, $lastName)
+    {
+
+        $person = new self;
+        $person->user = $user;
+        $person->lastName = $lastName;
+        $person->firstName = $firstName;
     }
 
     public function __set($name, $value)
