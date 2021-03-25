a5653939da15e6a7107f7a299216790ce5960d1c
damilolarandolph <damilolarandolph@gmail.com>
2021-03-25 10:31:47 +0000

    implemented email existence check.

--- src/data/dao/user.php
+++ src/data/dao/user.php
@@ -15,16 +15,35 @@ class UserDAO extends DAO
      * 
      * @param string $id Email of the user
      * 
-     * @return User
+     * @return User|boolean
      */
     public function findById($id)
     {
         $stmt = "SELECT * FROM {$this->table} where email=?";
         $prepedStmt = $this->conn->prepare($stmt);
         $prepedStmt->execute(array($id));
+        if ($prepedStmt->rowCount() < 1) {
+            return false;
+        }
         $prepedStmt->setFetchMode(PDO::FETCH_CLASS, "User");
         $result = $prepedStmt->fetch();
         return $result;
+    }
+
+    /**
+     * Checks is a user of the provided email exists in the database
+     * 
+     * @param string $email Email of the user to check
+     * 
+     * @return boolean
+     */
+    public function emailExists($email)
+    {
+        $result = $this->findById($email);
+        if ($this->findById($email) !== false) {
+            return true;
+        }
+        return false;
     }
 
     /**
