396412a146f4522ce98f7ffb41bd1a491b9ab0ec
damilolarandolph <damilolarandolph@gmail.com>
2021-03-25 10:37:11 +0000

    removed debug logs.

--- src/routes/signup.php
+++ src/routes/signup.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once  __DIR__ . "/../data/dao/person.php";
 require_once __DIR__ . "/../auth/session.php";
 require_once __DIR__ . "/../utils/form.php";
