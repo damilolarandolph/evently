4c1f4fe15da95210f29f08dd05d9f9d73619b9d0
damilolarandolph <damilolarandolph@gmail.com>
2021-04-17 20:03:38 +0000

    added countdown card html generator.

--- src/utils/templates.php
+++ src/utils/templates.php
@@ -42,3 +42,57 @@ function genEventPreviewCard($event)
                 </div>
             </a>";
 }
+
+
+/**
+ * Generates a countdown preview card for an event
+ * 
+ * @param Event $event
+ * 
+ * @return string
+ */
+
+function genEventCountdownCard($event)
+{
+
+    $startDate = date("jS F Y", $event->startTime);
+    $endDate = date("jS F Y", $event->endTime);
+    $endTime = date("g:iA", $event->endTime);
+    $startTime = date("g:iA", $event->startTime);
+    $endDate = $startDate === $endDate ? "" : " - " . $endDate;
+    $location = $event->isOnline ? "Remote" : $event->location;
+    $reservationsLeft = $event->tickets == 0 ? "Unlimited" : $event->getTicketsLeft();
+
+    echo " <a href='/event.php?event={$event->id}' class='upcoming-event'>
+            <main class='content'>
+                <h4 class='title'>{$event->name}</h4>
+                <img class='event-image' src='{$event->image}' />
+                <ul class='extra-details'>
+                    <li>
+                        <i class='far fa-clock'></i>
+                        {$startTime} - {$endTime}
+                    </li>
+                    <li>
+                        <i class='far fa-calendar-alt'></i>
+                            {$startDate}{$endDate}
+                    </li>
+                    <li>
+                        <i class='fas fa-map-marked-alt'></i>
+
+                            {$location}
+                    </li>
+                </ul>
+            </main>
+            <div class='kitchen-sink'>
+                <div class='item'>
+                    <p>Reservations Left</p>
+                    <h5>{$reservationsLeft}</h5>
+                </div>
+                <div class='item'>
+                    <p>Event registeration ends in</p>
+                    <h5 class='countdown' endTime='{$event->endTime}'></h5>
+                </div>
+
+            </div>
+        </a>";
+}
